4049ebd68adab1d5f4aca673d92ea8121c5511f1
FinnBoy <finn0918@163.com>
2018-11-22 23:36:05 +0800

    create infra.

---
+++ auth/src/main/java/net/awaken/auth/infrastructure/Authenticator.java
@@ -0,0 +1,20 @@
+package net.awaken.auth.infrastructure;
+
+/**
+ * 认证器
+ *
+ * @author Finn Zhao
+ * @version 1.0
+ * @since 11.05.2018
+ */
+public interface Authenticator {
+
+    /**
+     * authenticate token.
+     *
+     * @param authenticationToken token contains principal and credential.
+     * @return User or Session?
+     */
+    Object authenticate(Object authenticationToken);
+
+}
