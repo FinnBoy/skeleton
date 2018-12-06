95a08936faa82c5a116a92e35a3e5cf730dc03fe
FinnBoy <finn0918@163.com>
2018-12-07 00:28:47 +0800

    create infra.

--- auth/src/main/java/net/awaken/auth/server/domain/constant/Suitable.java
+++
@@ -1,9 +0,0 @@
-package net.awaken.auth.server.domain.constant;
-
-/**
- * match constant whether true or not.
- */
-public interface Suitable {
-
-    boolean match(Integer state);
-}
---
+++ auth/src/main/java/net/awaken/auth/server/service/Service.java
@@ -0,0 +1,52 @@
+package net.awaken.auth.server.service;
+
+import net.awaken.core.Request;
+import net.awaken.core.Response;
+
+/**
+ * Interface for delegation which will invoke actual handling.
+ *
+ * @author Finn Zhao
+ * @version 1.0
+ * @since 06.24.2018
+ */
+public interface Service {
+
+    Response execute(Request request);
+
+    /*~~~~~~~~ Resource ~~~~~~~~*/
+
+    void search();
+
+    void create();
+
+    void modifyParts();
+
+    void modify();
+
+    void remove();
+
+    /*~~~~~~~~ Service ~~~~~~~~*/
+
+    void perform();
+
+    void proceed();
+
+    void handle();
+
+    /*~~~~~~~~ Document ~~~~~~~~*/
+
+    void download();
+
+    void upload();
+
+    void upgrade();
+
+    void discard();
+
+    /*~~~~~~~~ Report ~~~~~~~~*/
+
+    void inline();
+
+    void attachment();
+}
