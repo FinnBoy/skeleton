aa8174dd68a637d6921b47e33834ed5b9a56a188
FinnBoy <finn0918@163.com>
2019-02-15 00:17:27 +0800

    create infra.

--- auth/src/main/java/net/awaken/auth/server/entity/resource/ResourceClosure.java
+++ auth/src/main/java/net/awaken/auth/server/entity/resource/ResourceClosure.java
@@ -3,6 +3,8 @@ package net.awaken.auth.server.entity.resource;
 import net.awaken.core.infrastructure.entity.EntityWithClosure;
 
 /**
+ * 资源树状结构
+ *
  * @author Finn Zhao
  * @version 1.0
  * @since 11.05.2018
