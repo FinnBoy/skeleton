7bdccd72941c6226d71fcb6e36b50d8d5b918246
FinnBoy <finn0918@163.com>
2019-02-15 00:43:01 +0800

    create infra.

---
+++ auth/src/main/java/net/awaken/auth/server/entity/permission/Permission.java
@@ -0,0 +1,11 @@
+package net.awaken.auth.server.entity.permission;
+
+/**
+ * @author Finn Zhao
+ * @version 1.0
+ * @since 12.01.2018
+ */
+public interface Permission {
+
+    Integer getType();
+}
